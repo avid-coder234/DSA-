2d85683b13987b5d6f91cbb971e9effcf66cf533
Aditya <riglunatic@gmail.com>
2024-01-07 23:29:48 +0530

    Add files via upload

---
+++ LEETCODE/majorityelement.cpp
@@ -0,0 +1,27 @@
+#include<vector.h>
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int ele;
+        for (int i = 0; i < nums.size(); i++) {
+            if (count == 0) {
+                count = 1;
+                ele = nums.at(i);
+            } else if (nums[i] == ele) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        int cnt;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == ele)
+                cnt++;
+
+            if (cnt > (nums.size()) / 2)
+                return ele;
+        }
+        return -1;
+    };
+};
