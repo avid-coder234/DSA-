afc86952645aa44c3f78350010e8b7fabefdb71b
Aditya <riglunatic@gmail.com>
2024-01-08 23:16:21 +0530

    Create IntersectionofTwoArrays.cpp

---
+++ LEETCODE/IntersectionofTwoArrays.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        int i=0,j=0;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+
+        while(i<nums1.size() && j<nums2.size())
+        {
+            if(i>0 && nums1[i]==nums1[i-1])
+            {
+                i++;
+                continue;
+            }
+
+            if(nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else if(nums1[i] == nums2[j])
+            {
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else 
+            {
+                j++;
+            }
+        }
+        return ans;
+
+    };
+};
