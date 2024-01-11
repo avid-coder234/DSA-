72464a3655bb5ba99d5f53e61d0a7ddc6fbcda96
Aditya <riglunatic@gmail.com>
2024-01-11 22:54:20 +0530

    Create searchinsert.cpp

---
+++ LEETCODE/searchinsert.cpp
@@ -0,0 +1,17 @@
+// First we sorted the array and  then by using for conditionals we found the target 
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target)
+    {
+        int adi = nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<adi;i++)
+        {
+            if(nums[i]== target)
+            return i;
+            else if(nums[i]>target)
+            return i;
+        }
+        return adi;
+    }
+};
